9fc48e6d0c8e8ed865f8577158c574f5661eb7dc
The-Ning <1558451297@qq.com>
2021-04-28 18:29:23 +0800

    按浏览量降序显示商品，主页

--- cloudfunctions/indexshop/index.js
+++ cloudfunctions/indexshop/index.js
@@ -4,5 +4,6 @@ cloud.init()
 const db = cloud.database();
 // 云函数入口函数
 exports.main = async (event, context) => {
-   return await db.collection('shoptable').get();
+   return await db.collection('shoptable').orderBy(
+      'clickload','desc').get();
 }
